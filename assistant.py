5522d49fc296142695ea31e7ccc062e492bd720f
Dylan Wolfe <wolfed9902@gmail.com>
2021-06-12 18:57:41 -0700

    Add func get_location()

--- assistant.py
+++ assistant.py
@@ -1,8 +1,17 @@
 # Virtuassistant
 # Created 6/10/21
 
+# Imports
+
+from bs4 import BeautifulSoup
 import pyttsx3
 import speech_recognition as sr
+import requests
+
+# Definitions
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36'}
+URL = ''
 
 # Initialize Text to Speech
 engine = pyttsx3.init('sapi5')
@@ -48,5 +57,20 @@ def voice_selection():
         listen_test()
     elif select == "2":
         speak("Have a nice day.")
+    elif select == "location":
+        print(get_location())
+
+def get_location():
+    try:
+        URL = 'https://iplocation.com/'
+        page = requests.get(URL, headers=headers)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        city = soup.find(class_='city').get_text()
+        region = soup.find(class_='region_name').get_text()
+        country = soup.find(class_='country_name').get_text()
+        return city, region, country
+    except Exception as e:
+        print('Error: location could not be retrieved.')
+        speak('Error, unknown location')
 
 voice_selection()
